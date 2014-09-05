9ea7f029cfa0cfaad53093a32231324d2b9a2f8c
Ryan Weaver <ryan@thatsquality.com>
2014-09-05 08:37:55 +0200

    [tuts-apply-diff] Applying existing diff file for step service-create-and-use-randomgenerator

---
+++ src/AppBundle/Service/RandomGenerator.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Service;
+
+class RandomGenerator
+{
+    public function generateString($length)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+    }
+}
