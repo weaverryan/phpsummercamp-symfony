eb2f17429937c7be03be8292093c6f2a9fd4a3ce
Ryan Weaver <ryan@thatsquality.com>
2014-09-05 08:37:54 +0200

    [tuts-apply-diff] Applying existing diff file for step basic-page-with-annotations

--- src/AppBundle/Controller/RandomController.php
+++ src/AppBundle/Controller/RandomController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RandomController
 {
+    /**
+     * @Route("/random/{limit}")
+     */
     public function indexAction($limit)
     {
         return new Response('<html><body>Number: '.rand(1, $limit).'</body></html>');
